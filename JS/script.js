07a5aca33643c23fc6bd4d82be20329e1deb6c5b
Jaskirat Oberoi <25457515+JaskiratOberoi@users.noreply.github.com>
2020-01-17 23:19:42 +0530

    fixed boxes js

--- JS/script.js
+++ JS/script.js
@@ -2,38 +2,37 @@ var calendar_blank_element = "";
 var calendar_filled_element = "";
 
 function filledboxes(filled) {
-  var j;
-  for (j = 0; j < filled; j++) {
-    calendar_filled_element += `<div class="calendar-block-filled"></div>`;
-  }
-  return calendar_filled_element;
+    var j;
+    calendar_filled_element = " ";
+    for (j = 0; j < filled; j++) {
+        calendar_filled_element += `<div class="calendar-block-filled"></div>`;
+    }
+    return calendar_filled_element;
 }
 
 function blankboxes(blank) {
-  var i;
-
-  for (i = 0; i < blank; i++) {
-    calendar_blank_element += `<div class="calendar-block"></div>`;
-  }
-  return calendar_blank_element;
+    var i;
+    calendar_blank_element = " ";
+    for (i = 0; i < blank; i++) {
+        calendar_blank_element += `<div class="calendar-block"></div>`;
+    }
+    return calendar_blank_element;
 }
 
 function valueInsert(leftWeeks, weekCount) {
-  calendar_empty = blankboxes(leftWeeks);
-  calendar_full = filledboxes(weekCount);
-  var calendar_boxes = calendar_full + calendar_empty;
-  document.getElementById("rows").innerHTML = calendar_boxes;
+    calendar_empty = blankboxes(leftWeeks);
+    calendar_full = filledboxes(weekCount);
+    var calendar_boxes = calendar_full + calendar_empty;
+    document.getElementById("rows").innerHTML = calendar_boxes;
 }
 
 function calendarCreation(age) {
-  var weekCount = age * 12;
-  var leftWeeks = 900 - weekCount;
-  valueInsert(leftWeeks, weekCount);
+    var weekCount = age * 12;
+    var leftWeeks = 900 - weekCount;
+    valueInsert(leftWeeks, weekCount);
 }
 
 function ageImporter() {
-  var userAge = document.getElementById(agevalue);
-  calendarCreation(userAge);
+    var userAge = document.getElementById("agevalue").value;
+    calendarCreation(userAge);
 }
-
-document.getElementById("ageButton").onclick = ageImporter;
